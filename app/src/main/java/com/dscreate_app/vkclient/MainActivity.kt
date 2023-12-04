e6c565381911eb7dba3e057d9679316350c36b08
DmitrySlv <dscreateapp@gmail.com>
2023-12-04 18:53:24 +0400

    Создание шапки поста.

--- app/src/main/java/com/dscreate_app/vkclient/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/vkclient/MainActivity.kt
@@ -3,13 +3,17 @@ package com.dscreate_app.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.dscreate_app.vkclient.ui.theme.VkClientTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,15 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             VkClientTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(MaterialTheme.colorScheme.background)
+                        .padding(8.dp)
                 ) {
-                    Greeting("Android")
+                    PostCard()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    VkClientTheme {
-        Greeting("Android")
     }
 }
