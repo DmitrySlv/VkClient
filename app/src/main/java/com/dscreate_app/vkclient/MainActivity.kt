839b33b7071dd2d43c51213700aab3e084d7a7ae
DmitrySlv <dscreateapp@gmail.com>
2023-12-05 19:35:06 +0400

    Создание Scaffold (контейнер на весь экран), TopAppBar, BottomAppBar и DrawerMenu.

--- app/src/main/java/com/dscreate_app/vkclient/MainActivity.kt
+++ app/src/main/java/com/dscreate_app/vkclient/MainActivity.kt
@@ -3,15 +3,37 @@ package com.dscreate_app.vkclient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material.icons.outlined.Edit
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.OutlinedButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MovableContent
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
@@ -43,59 +65,5 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Test() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
-    ) {
-        Example3()
-    }
-}
-
-@Composable
-private fun Example1() {
-    OutlinedButton(
-        onClick = {  }
-    ) {
-        Text(text = "Hello World")
-    }
-}
-
-@Composable
-private fun Example2() {
-    var text by rememberSaveable {
-        mutableStateOf("")
-    }
-    TextField(
-        value = text,
-        onValueChange = { text = it },
-        label = { Text(text = "Label") },
-        singleLine = true
-    )
-}
-
-@Composable
-private fun Example3() {
-    AlertDialog(
-        onDismissRequest = { },
-        confirmButton = {
-            Text(
-                modifier = Modifier.padding(8.dp),
-                text = "Да"
-            )
-        },
-        title = {
-            Text(text = "Вы уверены?")
-        },
-        text = {
-            Text(text = "Вы действительно хотите удалить этот файл?")
-        },
-        dismissButton = {
-            TextButton(
-                onClick = {
-                }
-            ) {
-                Text(text = "Нет")
-            }
-        }
-    )
+   Drawer()
 }
