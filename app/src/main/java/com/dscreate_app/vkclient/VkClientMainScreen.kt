bb17df0fbd46362064f48c6dbb59abe702a3333f
DmitrySlv <dscreateapp@gmail.com>
2023-12-06 16:57:34 +0400

    Использование FAB и SnackBar и работа с состояниями.

--- app/src/main/java/com/dscreate_app/vkclient/VkClientMainScreen.kt
+++ app/src/main/java/com/dscreate_app/vkclient/VkClientMainScreen.kt
@@ -1,19 +1,62 @@
 package com.dscreate_app.vkclient
 
 import android.util.Log
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.material3.SnackbarResult
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 @Composable
 fun MainScreen() {
+    val snackbarHostState = SnackbarHostState()
+    val scope = rememberCoroutineScope()
+    val fabIsVisible = remember {
+        mutableStateOf(true)
+    }
+
     Scaffold(
+        snackbarHost = {
+            SnackbarHost(hostState = snackbarHostState)
+        },
+        floatingActionButton = {
+                        if (fabIsVisible.value) {
+                            FloatingActionButton(
+                                shape = CircleShape,
+                                onClick = {
+                                    scope.launch { // Запуск снэкбара только из корутины для компоуза
+                                        val action = snackbarHostState.showSnackbar(
+                                            message = "Это snackbar",
+                                            actionLabel = "Спрятать FAB", //Кнопка в snackbar для скрытия snackbar
+                                            duration = SnackbarDuration.Long //длительность snackbar
+                                        )
+                                        if (action == SnackbarResult.ActionPerformed) {
+                                            fabIsVisible.value = false
+                                        }
+                                    }
+                                }
+                            ) {
+                                Icon(Icons.Filled.Favorite, contentDescription = null)
+                            }
+                        }
+        },
         bottomBar = {
             NavigationBar {
                 Log.d("MyLog", "NavigationBar")
